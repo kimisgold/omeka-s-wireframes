d8afa03f541ae4b9eb53b8d37b63c6c32bd94c72
Kim Nguyen <design@kimisgold.com>
2014-06-19 14:38:27 -0400

    Make root directory configurable.

--- header.php
+++ header.php
@@ -1,17 +1,18 @@
+<?php $root = ''; // Set this variable for your local directory structure. ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Omeka S</title>
-    <link href="/~kim/omeka-s-wireframes/css/screen.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo $root; ?>/omeka-s-wireframes/css/screen.css" rel="stylesheet" type="text/css">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="/~kim/omeka-s-wireframes/javascript/global.js"></script>
+    <script src="<?php echo $root; ?>/omeka-s-wireframes/javascript/global.js"></script>
     <?php echo $js; ?>
 </head>
 <body class="<?php echo $bodyClass; ?>">
 <header>
-    <div class="logo"><a href="#">Omeka S</a></div>
+    <div class="logo"><a href="<?php echo $root; ?>/omeka-s-wireframes/">Omeka S</a></div>
     <div id="current-user">
         <p>Welcome, <a href="#">Admin</a></p>
         <p><a href="#">Logout</a></p>
@@ -23,9 +24,9 @@
     </div>
     <nav id="primary-nav">
         <ul>
-            <li><a href="/~kim/omeka-s-wireframes/items/browse" class="icon-items">Items</a></li>
+            <li><a href="<?php echo $root; ?>/omeka-s-wireframes/items/browse" class="icon-items">Items</a></li>
             <li><a href="#" class="icon-item-sets">Item Sets</a></li>
-            <li><a href="/~kim/omeka-s-wireframes/vocabularies/" class="icon-vocabularies">Vocabularies</a></li>
+            <li><a href="<?php echo $root; ?>/omeka-s-wireframes/vocabularies/" class="icon-vocabularies">Vocabularies</a></li>
             <li><a href="#" class="icon-media">Media</a></li>
             <li><a href="#" class="icon-modules">Modules</a></li>
             <li><a href="#" class="icon-users">Users</a></li>
