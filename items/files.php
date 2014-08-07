34aa6e7e30c4673f98fc202c05f332cccb09e8e0
Kim Nguyen <design@kimisgold.com>
2014-08-07 14:12:17 -0400

    Move input tabs to top from side, style media section of add/edit items page.

--- items/files.php
+++ items/files.php
@@ -1,41 +1,8 @@
 <div id="item-media" class="section">
-    <div class="existing-files">
-    </div>
-    <div class="upload-zone">
-        <p>Drag file here or</p>
-        <button>Choose file to upload</button>
-    </div>
-    <div class="field">
-        <div class="field-meta">
-            <label for="image-url">
-                Image URL
-            </label>
-        </div>
-        <div class="inputs">
-            <input type="text" id="image-url" name="image-url">
-        </div>
-    </div>
-    <div class="field">
-        <div class="field-meta">
-            <label for="youtube-code">
-                Youtube embed code
-            </label>
-        </div>
-        <div class="inputs">
-            <textarea name="youtube-code" id="youtube-code" rows="3"></textarea>
-        </div>
-    </div>
-    <div class="field">
-        <div class="field-meta">
-            <label for="flickr-code">
-                Flickr embed code
-            </label>
-        </div>
-        <div class="inputs">
-            <textarea name="flickr-code" id="flickr-code" rows="3"></textarea>
-        </div>
-    </div>
     <div class="media field">
+        <div class="field-actions">
+            <a href="#" class="add-value button fa-plus"><span class="screen-reader-text">Add Value</span></a>
+        </div>
         <div class="field-meta">
             <label>
                 Media File
@@ -49,9 +16,20 @@
                     <a link="#" class="tab fa-flickr"><span class="screen-reader-text">Flickr</span></a>
                     <a link="#" class="tab fa-youtube"><span class="screen-reader-text">Youtube</span></a>
                 </div>
-                <div class="active type input-option">
+                <div class="active upload input-option">
+                    <div class="existing-files">
+                    </div>
+                    <div class="upload-zone">
+                        <p>Drag file here or</p>
+                        <button>Choose file to upload</button>
+                    </div>
+                </div>
+                <div class="type input-option">
                     <textarea name="dc-title" rows="1"></textarea>
                     </select>
+                </div>
+                <div class="flickr input-option">
+                    <textarea name="flickr-code" id="flickr-code" rows="3" placeholder="Flickr embed code"></textarea>
                 </div>
                 <div class="items input-option">
                     <span class="default">No item selected</span>
@@ -60,8 +38,49 @@
                 <div class="link input-option">
                     <input type="text" name="dc-title" placeholder="Enter URL">
                 </div>
+                <div class="youtube input-option">
+                    <textarea name="youtube-code" id="youtube-code" rows="3" placeholder="Youtube embed code"></textarea>
+                </div>
                 <button class="remove-value">Remove value</button>
             </div>
-</div>
+        </div>
+    </div>
+    <div class="new item-media field template">
+        <div class="inputs">
+            <div class="value">
+                <div class="input-options">
+                    <a link="#" class="tab active fa-upload"><span class="screen-reader-text">Upload</span></a>
+                    <a link="#" class="tab fa-link"><span class="screen-reader-text">Url</span></a>
+                    <a link="#" class="tab fa-flickr"><span class="screen-reader-text">Flickr</span></a>
+                    <a link="#" class="tab fa-youtube"><span class="screen-reader-text">Youtube</span></a>
+                </div>
+                <div class="active upload input-option">
+                    <div class="existing-files">
+                    </div>
+                    <div class="upload-zone">
+                        <p>Drag file here or</p>
+                        <button>Choose file to upload</button>
+                    </div>
+                </div>
+                <div class="type input-option">
+                    <textarea name="dc-title" rows="1"></textarea>
+                    </select>
+                </div>
+                <div class="flickr input-option">
+                    <textarea name="flickr-code" id="flickr-code" rows="3" placeholder="Flickr embed code"></textarea>
+                </div>
+                <div class="items input-option">
+                    <span class="default">No item selected</span>
+                    <a href="#item-select" class="modal-link button">Select Omeka Item</a>
+                </div>
+                <div class="link input-option">
+                    <input type="text" name="dc-title" placeholder="Enter URL">
+                </div>
+                <div class="youtube input-option">
+                    <textarea name="youtube-code" id="youtube-code" rows="3" placeholder="Youtube embed code"></textarea>
+                </div>
+                <button class="remove-value active">Remove value</button>
+            </div>
+        </div>
     </div>
 </div>
