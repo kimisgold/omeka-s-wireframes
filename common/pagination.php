4b54f0b3524d2115deb034e4722ae0aa084a9d2f
Kim Nguyen <design@kimisgold.com>
2014-08-11 11:33:13 -0400

    Abstract pagination out as its own partial.

---
+++ common/pagination.php
@@ -0,0 +1,10 @@
+<nav class="pagination" role="navigation">
+    <form>
+        <input type="text" name="page-input-top" id="page-input-top" value="1" size="4">
+        <span class="page-count">of 3</span>
+    </form>
+    <a href="#" class="first fa-angle-double-left button"><span class="screen-reader-text">First</span></a>
+    <a href="#" class="previous fa-angle-left button"><span class="screen-reader-text">Previous</span></a>
+    <a href="#" class="next fa-angle-right button"><span class="screen-reader-text">Next</span></a>
+    <a href="#" class="last fa-angle-double-right button"><span class="screen-reader-text">Last</span></a>
+</nav>
--- items/browse.php
+++ items/browse.php
@@ -6,16 +6,7 @@
 <a href="add" class="add button">Add new item</a>
 <a href="batch-edit" class="batch-edit button">Edit selected items</a>
 
-<nav class="pagination" role="navigation">
-    <form>
-        <input type="text" name="page-input-top" id="page-input-top" value="1" size="4">
-        <span class="page-count">of 3</span>
-    </form>
-    <a href="#" class="first fa-angle-double-left button"><span class="screen-reader-text">First</span></a>
-    <a href="#" class="previous fa-angle-left button"><span class="screen-reader-text">Previous</span></a>
-    <a href="#" class="next fa-angle-right button"><span class="screen-reader-text">Next</span></a>
-    <a href="#" class="last fa-angle-double-right button"><span class="screen-reader-text">Last</span></a>
-</nav>
+<?php include('../common/pagination.php'); ?>
 
 <table id="items-list">
     <thead>
